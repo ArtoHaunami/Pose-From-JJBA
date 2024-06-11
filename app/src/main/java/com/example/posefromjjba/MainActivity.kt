1e0ac3f8ec08af137a3a29b1b7838e3d4582eaa1
ArtoHaunami <lucky_spyro@mail.ru>
2024-06-11 21:06:57 +0300

    Change MainActivity

--- app/src/main/java/com/example/posefromjjba/MainActivity.kt
+++ app/src/main/java/com/example/posefromjjba/MainActivity.kt
@@ -2,40 +2,23 @@ package com.example.posefromjjba
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
-package com.example.posefromjjba
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager import androidx.recyclerview.widget.RecyclerView
+
 class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState) setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView) recyclerView.layoutManager = LinearLayoutManager(this)
-        val datalist = listOf(
-            DataModel("Item 1"),
-            DataModel("Item 2"),
-            DataModel("Item 3"),
-            DataModel("Item 4")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Picasso
+            .get()
+            .load()
+            .into(binding.image);
     }
 }
 val adapter = MyAdapter(datalist) recyclerView.adapter = adapter
-
-
-    class MainActivity: AppCompatActivity() {
-
-        private lateinit var binding: ActivityMainBinding
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-            Picasso
-                .get()
-                .load()
-                .into(binding.image);
-        }
-
-    }
-
